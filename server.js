27a9df8cca64680631579828e5379221f3075a95
Kai <kaiann.fletcher@mail.utoronto.ca>
2020-07-01 01:25:47 -0400

    Static server files.

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ require("./config/passport")(passport);
 
 //Send every other request to the React app
 //Define any API routes before this runs
-app.get("/", (req, res) => {
+app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
