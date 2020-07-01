0e3bc4ddcc9a34d51a37fb10a5ab7bcfae4f0015
Kai <kaiann.fletcher@mail.utoronto.ca>
2020-06-30 23:53:07 -0400

    Fixing page serving issues.

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ console.log("routes:",routes);
 
 //Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static("client/build"));
+    app.use(express.static(path.join(__dirname, "client/build")));
 }
 
 // Connect to the Mongo DB 
