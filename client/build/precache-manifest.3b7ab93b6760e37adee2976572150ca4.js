4fb9b7ddb41134ed65ded7dec53f812a61b74aeb
Kai <kaiann.fletcher@mail.utoronto.ca>
2020-07-01 19:04:15 -0400

    Trying to fix static serving.

---
+++ client/build/precache-manifest.3b7ab93b6760e37adee2976572150ca4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01bd9fe3ac97f0536bf426e037967cf9",
+    "url": "./index.html"
+  },
+  {
+    "revision": "90b86456d38e39cd0115",
+    "url": "./static/css/main.20e70fe6.chunk.css"
+  },
+  {
+    "revision": "802695d2842e186224a8",
+    "url": "./static/js/2.f61f68d2.chunk.js"
+  },
+  {
+    "revision": "56729d980da09961afe8d497a21eec12",
+    "url": "./static/js/2.f61f68d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "90b86456d38e39cd0115",
+    "url": "./static/js/main.1e91dedb.chunk.js"
+  },
+  {
+    "revision": "1e8c7afc7bf36ad0e5f1",
+    "url": "./static/js/runtime-main.64664b1b.js"
+  }
+]);
--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.22c0ab14a9282a70a917dd7954dcbfd1.js"
+  "./precache-manifest.3b7ab93b6760e37adee2976572150ca4.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
