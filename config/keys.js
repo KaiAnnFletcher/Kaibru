1d5cb459869852ec5af2fade33564d738086bf1a
Kai <kaiann.fletcher@mail.utoronto.ca>
2020-07-01 18:03:45 -0400

    removed build folder from gitignore.

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-    MONGODB_URI: "mongodb://heroku_tk82nh0f:n5hfgf89jt26lrna7pj4fe2b04@ds035488.mlab.com:35488/heroku_tk82nh0f",
+    //MONGODB_URI: "mongodb://heroku_tk82nh0f:n5hfgf89jt26lrna7pj4fe2b04@ds035488.mlab.com:35488/heroku_tk82nh0f",
     secretOrKey: "secret"
   };
