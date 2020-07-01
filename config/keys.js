3b381f70df5ccbf45d563df5e8354e882d2f7f9b
Kai <kaiann.fletcher@mail.utoronto.ca>
2020-07-01 17:53:38 -0400

    MONGODB_URI

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-    //mongoURI: "YOUR_MONGO_URI_HERE",
+    MONGODB_URI: "mongodb://heroku_tk82nh0f:n5hfgf89jt26lrna7pj4fe2b04@ds035488.mlab.com:35488/heroku_tk82nh0f",
     secretOrKey: "secret"
   };
