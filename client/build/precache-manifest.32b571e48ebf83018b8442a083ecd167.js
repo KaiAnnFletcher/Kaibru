e23b7271caa8d1680e1cdf9d604e5dba35d055be
Kai <kaiann.fletcher@mail.utoronto.ca>
2020-07-01 22:58:59 -0400

    Static file fix.

---
+++ client/build/precache-manifest.32b571e48ebf83018b8442a083ecd167.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbf0675665694de480561cecb668ac10",
+    "url": "/client/index.html"
+  },
+  {
+    "revision": "1167916d1207ae158f1d",
+    "url": "/client/static/css/main.20e70fe6.chunk.css"
+  },
+  {
+    "revision": "802695d2842e186224a8",
+    "url": "/client/static/js/2.f61f68d2.chunk.js"
+  },
+  {
+    "revision": "56729d980da09961afe8d497a21eec12",
+    "url": "/client/static/js/2.f61f68d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1167916d1207ae158f1d",
+    "url": "/client/static/js/main.3eb3d29b.chunk.js"
+  },
+  {
+    "revision": "78fa274c5a6a557ea74f",
+    "url": "/client/static/js/runtime-main.b5bbee55.js"
+  }
+]);
