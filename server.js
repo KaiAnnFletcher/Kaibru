fe1e2cd091b725ccdcf33796f20df2888dac9968
Kai <kaiann.fletcher@mail.utoronto.ca>
2020-07-03 02:07:27 -0400

    File pathing updated.

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ console.log("routes:",routes);
 
 //Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === 'production') {
-   app.use(express.static('/client/build/static'));
+   app.use(express.static('/client/build/static/js'));
 }
 
 app.use(passport.initialize());
